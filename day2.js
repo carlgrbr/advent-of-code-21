8cfc35f3bb14c710efbf5cde9f1cc114b9b0499c
carlgrbr <52937313+carlgrbr@users.noreply.github.com>
2021-12-03 17:46:49 +0100

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,16 +1,16 @@
-Task 2: 
-
 const input =  ["up 6", "down 7", "forward 5", "forward 5", "down 9", "down 5", "up 4", "forward 1", "forward 7", "down 8"]
 
-const formattedData = input.map(e => {
-    const [direction, value] = e.split(' ');
-    return { direction, value: Number(value) }
-})
+const solveTask2 = (input) => {
+  const formattedData = input.map(e => {
+      const [direction, value] = e.split(' ');
+      return { direction, value: Number(value) }
+  })
 
-const result = formattedData.reduce(({length, depth, aim}, {direction, value}) => ({
-    length: direction === 'forward' ? length + value : length,
-    depth: direction === 'forward' ? depth + value * aim : depth,
-    aim: direction === 'forward' ? aim : direction === 'down' ? aim + value : aim - value
-}), { length: 0, depth: 0, aim: 0 })
+  const result = formattedData.reduce(({length, depth, aim}, {direction, value}) => ({
+      length: direction === 'forward' ? length + value : length,
+      depth: direction === 'forward' ? depth + value * aim : depth,
+      aim: direction === 'forward' ? aim : direction === 'down' ? aim + value : aim - value
+  }), { length: 0, depth: 0, aim: 0 })
 
-return result.length * result.depth
+  return result.length * result.depth
+}
